1c5fb4fbef8ab167bf67c9f7a0c4acd317ee9023
angelnvshen <angelnvshen@163.com>
2017-04-09 12:06:56 +0800

    spring security userdetail use jdbc

---
+++ lawCloudLawper/src/main/resources/sql/security.sql
@@ -0,0 +1,37 @@
+create table users (
+    username varchar(50) not null primary key,
+    password varchar(50) not null,
+    enabled boolean not null
+);
+
+create table authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key (username)
+        references users (username)
+);
+      create unique index ix_auth_username on authorities (username,authority);
+
+
+
+create table groups (
+    id bigint NOT NULL AUTO_INCREMENT,
+    group_name varchar(50) NOT NULL,
+    PRIMARY KEY (`Id`)
+);
+
+create table group_authorities (
+    group_id bigint NOT NULL,
+    authority varchar(50) NOT NULL,
+    constraint fk_group_authorities_group foreign key (group_id)
+        references groups (id)
+);
+
+create table group_members (
+    id bigint NOT NULL AUTO_INCREMENT,
+    username varchar(50) NOT NULL,
+    group_id bigint NOT NULL,
+    PRIMARY KEY (`Id`),
+    constraint fk_group_members_group foreign key (group_id)
+        references groups (id)
+);
