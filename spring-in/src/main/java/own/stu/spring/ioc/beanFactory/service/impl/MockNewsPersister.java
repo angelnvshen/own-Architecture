084449bcb7cc6d190a34d6525ab4b5dfafa9b5b7
angelnvshen <angelnvshen@163.com>
2017-06-02 13:09:14 +0800

    ObjectFactoryCreatingFactoryBean 隔离了客户端对象对BeanFactory的直接引用

--- spring-in/src/main/java/own/stu/spring/ioc/beanFactory/service/impl/MockNewsPersister.java
+++ spring-in/src/main/java/own/stu/spring/ioc/beanFactory/service/impl/MockNewsPersister.java
@@ -3,6 +3,7 @@ package own.stu.spring.ioc.beanFactory.service.impl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.ObjectFactory;
 import own.stu.spring.ioc.beanFactory.model.FXNewsBean;
 import own.stu.spring.ioc.beanFactory.service.IFXNewsPersister;
 
@@ -10,8 +11,9 @@ import own.stu.spring.ioc.beanFactory.service.IFXNewsPersister;
  * 使用 BeanFactoryAware 接口
  * 能够保证每次调用 BeanFactory 的 getBean("newsBean") ，就同样可以每次都取得新的 FXNewsBean 对象实例
  */
-public class MockNewsPersister implements IFXNewsPersister,BeanFactoryAware {
-    private BeanFactory beanFactory;
+public class MockNewsPersister implements IFXNewsPersister {
+
+    private ObjectFactory newsBeanFactory;
 
     public void persistNews(FXNewsBean bean) {
         persistNews();
@@ -22,10 +24,10 @@ public class MockNewsPersister implements IFXNewsPersister,BeanFactoryAware {
     }
 
     public FXNewsBean getNewsBean() {
-        return (FXNewsBean) beanFactory.getBean("newsBean");
+        return (FXNewsBean) newsBeanFactory.getObject();
     }
 
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
+    public void setNewsBeanFactory(ObjectFactory newsBeanFactory) {
+        this.newsBeanFactory = newsBeanFactory;
     }
 }
