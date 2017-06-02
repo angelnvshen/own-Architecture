f47ffc8f71653a9b0150fb9c817c70368b08f306
angelnvshen <angelnvshen@163.com>
2017-06-02 11:54:20 +0800

    beanFactoryAware 接接口

--- spring-in/src/main/java/own/stu/spring/ioc/beanFactory/service/impl/MockNewsPersister.java
+++ spring-in/src/main/java/own/stu/spring/ioc/beanFactory/service/impl/MockNewsPersister.java
@@ -1,10 +1,17 @@
 package own.stu.spring.ioc.beanFactory.service.impl;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import own.stu.spring.ioc.beanFactory.model.FXNewsBean;
 import own.stu.spring.ioc.beanFactory.service.IFXNewsPersister;
 
-public class MockNewsPersister implements IFXNewsPersister {
-    private FXNewsBean newsBean;
+/**
+ * 使用 BeanFactoryAware 接口
+ * 能够保证每次调用 BeanFactory 的 getBean("newsBean") ，就同样可以每次都取得新的 FXNewsBean 对象实例
+ */
+public class MockNewsPersister implements IFXNewsPersister,BeanFactoryAware {
+    private BeanFactory beanFactory;
 
     public void persistNews(FXNewsBean bean) {
         persistNews();
@@ -15,10 +22,10 @@ public class MockNewsPersister implements IFXNewsPersister {
     }
 
     public FXNewsBean getNewsBean() {
-        return newsBean;
+        return (FXNewsBean) beanFactory.getBean("newsBean");
     }
 
-    public void setNewsBean(FXNewsBean newsBean) {
-        this.newsBean = newsBean;
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 }
